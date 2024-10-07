d99f8d9dc49f36f2d20456c1237a8f0c26848793
vinayaksingh262 <vs2622003@gmail.com>
2024-10-07 14:58:37 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,23 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
 
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
 
+        return not stack
 
+# Example usage:
+solution = Solution()
 
-
-
-
-    
-
-
-
-  
-
+print(solution.isValid("()"))       # Output: True
+print(solution.isValid("()[]{}"))   # Output: True
+print(solution.isValid("(]"))       # Output: False
+print(solution.isValid("([)]"))     # Output: False
+print(solution.isValid("{[]}"))     # Output: True
